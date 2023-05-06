0a002de1167640a066cae90e35e6fd315216e7b1
Squirrel68 <constantlypeacinside@gmail.com>
2023-05-06 21:15:09 +0700

    after l9 in class

--- src/apps/controllers/admin.js
+++ src/apps/controllers/admin.js
@@ -1,5 +1,9 @@
-const index = (req, res) => {
-  res.send("admin-dashboard");
+const UserModel = require("../models/user");
+const ProductModel = require("../models/product");
+const index = async (req, res) => {
+  const users = (await UserModel.find()).length;
+  const products = (await ProductModel.find()).length;
+  res.render("admin/dashboard", { users, products });
 };
 
 module.exports = {
--- src/apps/controllers/product.js
+++ src/apps/controllers/product.js
@@ -1,11 +1,14 @@
-const index = (req, res) => {
-  res.send("products");
+const ProductModel = require("../models/product");
+const index = async (req, res) => {
+  const products = await ProductModel.find().populate({ path: "cat_id" });
+  // console.log(products);
+  res.render("admin/products/product", { products });
 };
 const create = (req, res) => {
-  res.send("create");
+  res.render("admin/products/add_product");
 };
 const edit = (req, res) => {
-  res.send("edit");
+  res.render("admin/products/edit_product");
 };
 const del = (req, res) => {
   res.send("del");
